5cc9bd732bdcc1f1b5954677e65e9072e2a20796
Andrew Barba <barba@hey.com>
2023-02-02 12:56:52 -0500

    Cleanup

--- Sources/Ed25519/Seed.swift
+++ Sources/Ed25519/Seed.swift
@@ -1,15 +1,17 @@
+private let secureSeedLength = 32
+
 public struct Seed: Sendable {
     public let bytes: [UInt8]
 
     public init(bytes: [UInt8]) throws {
-        guard bytes.count == 32 else {
+        guard bytes.count == secureSeedLength else {
             throw Ed25519Error.invalidSeedLength
         }
         self.bytes = bytes
     }
 
     public init() throws {
-        let bytes = [UInt8](repeating: 0, count: 32).map { _ in
+        let bytes = Array(repeating: 0, count: secureSeedLength).map { _ in
             return UInt8.random(in: UInt8.min ... UInt8.max)
         }
         try self.init(bytes: bytes)
